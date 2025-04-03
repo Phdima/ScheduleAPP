336a5c0bc2a302c88c2ee6542c93b1db0733b26c
Phdima <dmitrytimonin4@gmail.com>
2025-04-03 22:36:23 +0700

    сделал супер черновую версию отображения UI имеются ошибка с вылетом приложения при выставлении даты в прошедем времени

--- app/src/main/java/com/example/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -7,10 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.scheduleapp.presentation.screens.MainScreen
 import com.example.scheduleapp.ui.theme.ScheduleAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -20,7 +22,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            ScheduleAppTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/scheduleapp/presentation/screens/MainScreen.kt
+++ app/src/main/java/com/example/scheduleapp/presentation/screens/MainScreen.kt
@@ -1,2 +1,54 @@
-package com.example.scheduleapp.presentation
+@file:OptIn(ExperimentalMaterial3Api::class)
 
+package com.example.scheduleapp.presentation.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.scheduleapp.domain.model.ScheduleEvent
+import com.example.scheduleapp.presentation.viewModels.ScheduleVM
+
+
+@Composable
+fun MainScreen() {
+    val viewModel: ScheduleVM = hiltViewModel()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    Scaffold(
+        topBar = { TopAppBar(title = { Text("Главный экран") }) },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                viewModel.addEvent()
+            }) {
+                Icon(Icons.Default.Add, "Добавить")
+            }
+        },
+
+        ) { padding ->
+        ScheduleCard(
+            state = state,
+            onTitleChange = viewModel::updateTitle,
+            onDescriptionChange = viewModel::updateDescription,
+            onTimeChange = viewModel::updateStartTime,
+            modifier = Modifier.padding(padding)
+        )
+
+    }
+}
