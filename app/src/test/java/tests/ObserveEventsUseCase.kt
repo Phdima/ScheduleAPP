ef3ef15c7b1574e82a0bc4ac3e12fac9acd126a9
Phdima <dmitrytimonin4@gmail.com>
2025-04-12 14:15:36 +0700

    написал тесты для ObserveEventsUseCase

---
+++ app/src/test/java/tests/ObserveEventsUseCase.kt
@@ -0,0 +1,68 @@
+package tests
+
+import app.cash.turbine.test
+import com.example.scheduleapp.data.room.dao.ScheduleDao
+import com.example.scheduleapp.domain.model.ScheduleEvent
+import com.example.scheduleapp.domain.repository.ScheduleRepository
+import com.example.scheduleapp.domain.useCases.AddEventUseCase
+import com.example.scheduleapp.domain.useCases.ObserveEventsUseCase
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Instant
+import org.junit.Before
+import kotlin.test.Test
+
+
+class ObserveEventsUseCase {
+
+
+    @Test
+    fun `invoke() should call observeUpcomingEvents from repository`() = runTest {
+
+        val mockRepo = mockk<ScheduleRepository>()
+        val useCase = ObserveEventsUseCase(mockRepo)
+
+        coEvery { mockRepo.observeUpcomingEvents() } returns emptyFlow()
+
+
+        useCase().toList()
+
+
+        coVerify(exactly = 1) { mockRepo.observeUpcomingEvents() }
+    }
+
+    @Test
+    fun `invoke() should emit data from repository`() = runTest {
+
+        val mockRepo = mockk<ScheduleRepository>()
+        val useCase = ObserveEventsUseCase(mockRepo)
+        val testData = listOf(
+            ScheduleEvent(
+                id = 1,
+                startTime = Instant.parse("2024-01-01T10:00:00Z"),
+                title = "Test",
+                description = "Test",
+            ),
+            ScheduleEvent(
+                id = 2,
+                startTime = Instant.parse("2024-01-01T12:00:00Z"),
+                title = "Test",
+                description = "Test",
+            ),
+        )
+        coEvery { mockRepo.observeUpcomingEvents() } returns flowOf(testData)
+
+
+        useCase().test {
+            assertEquals(testData, awaitItem())
+            awaitComplete()
+        }
+    }
+}
